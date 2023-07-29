1fa56d2ef4d6a77ea09305cf3bc26a991393a4e1
JoshiNagendra <109235351+JoshiNagendra@users.noreply.github.com>
2023-07-29 17:06:48 +0530

    Newly updated by joshi

--- SecondClass.java
+++ SecondClass.java
@@ -5,6 +5,10 @@ public class SecondClass {
 	public void display() {
 		System.out.println("Display method in the Second class");
 	}
+
+	public void Show(){
+             System.out.println("This is show method ");
+	}
 	
 	public String getResult() {
 	
