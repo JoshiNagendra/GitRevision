7811a676f5d2ccb027f957001fc19998a5a9f615
JoshiNagendra <joshichennamsetti999@gmail.com>
2023-07-29 16:45:32 +0530

    commited by joshi

---
+++ FirstClass.java
@@ -0,0 +1,19 @@
+package in.joshi.model;
+
+import java.util.ArrayList;
+
+public class FirstClass {
+
+	static SecondClass second;
+
+	public static void main(String[] args) {
+                System.out.println("Welcome to modified file");
+
+		Integer a = 10;
+		int b=20;
+		System.out.println(b);
+		System.out.println(a);
+		
+
+	}
+}
---
+++ SecondClass.java
@@ -0,0 +1,14 @@
+package in.joshi.model;
+
+public class SecondClass {
+	
+	public void display() {
+		System.out.println("Display method in the Second class");
+	}
+	
+	public String getResult() {
+	
+		return "Joshi";
+	}
+
+}
