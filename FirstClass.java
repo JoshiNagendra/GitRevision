97d681f6b31a901d817bdd63c0ca4b67a354902b
JoshiNagendra <joshichennamsetti999@gmail.com>
2023-07-29 17:05:06 +0530

    Next commit by Joshi

--- FirstClass.java
+++ FirstClass.java
@@ -6,8 +6,17 @@ public class FirstClass {
 
 	static SecondClass second;
 
+
+        public void show(){
+        System.out.println("It is not a normal show method it a show method");
+
+
+        }
+
 	public static void main(String[] args) {
                 System.out.println("Welcome to modified file");
+
+                
 
 		Integer a = 10;
 		int b=20;
